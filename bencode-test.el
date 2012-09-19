bb2a072dd3984a32c038695f86a097a204c0b2b3
Christopher Wellons <mosquitopsu@gmail.com>
2012-09-19 16:30:20 -0400

    Change lists to vectors to fix ambiguity.
    A dictionary with list or dictionary as a value was ambiguous. For
    example:
    
        "d0:li0eee" => (("" 0)) => "ll0:i0eee".
    
    A list containing a list or a dictionary with a list as a value?

--- bencode-test.el
+++ bencode-test.el
@@ -10,21 +10,16 @@
   (should (string= (bdecode-string "0:") "")))
 
 (ert-deftest bdecode-list ()
-  (should (equal (bdecode-string "li5ee") '(5)))
-  (should (equal (bdecode-string "le") '()))
-  (should (equal (bdecode-string "lli0eee") '((0))))
-  (should (equal (bdecode-string "l3:fooi3ee") '("foo" 3))))
+  (should (equal (bdecode-string "li5ee") [5]))
+  (should (equal (bdecode-string "le") []))
+  (should (equal (bdecode-string "lli0eee") [[0]]))
+  (should (equal (bdecode-string "l3:fooi3ee") ["foo" 3])))
 
 (ert-deftest bdecode-dict ()
   (should (equal (bdecode-string "de") '()))
   (should (equal (bdecode-string "d3:foo3:bare") '(("foo" . "bar"))))
   (should (equal (bdecode-string "d5:helloi5ee") '(("hello" . 5))))
   (should (equal (bdecode-string "d0:d3:fooi0eee") '(("" ("foo" . 0))))))
-
-(ert-deftest improperp ()
-  (should (improperp '(1 . 2)))
-  (should (improperp '(1 2 . 3)))
-  (should-not (improperp '(1 2 3))))
 
 (ert-deftest bencode--int ()
   (should (string= (bencode--int -0) "i0e"))
@@ -38,11 +33,13 @@
   (should (string= (bencode--string "�") "3:�")))
 
 (ert-deftest bencode--list ()
-  (should (string= (bencode--list '(5 "foo")) "li5e3:fooe"))
-  (should (string= (bencode--list '()) "le"))
-  (should (string= (bencode--list '(2 ("bar" -3))) "li2el3:bari-3eee")))
+  (should (string= (bencode--list [5 "foo"]) "li5e3:fooe"))
+  (should (string= (bencode--list []) "le"))
+  (should (string= (bencode--list [2 ["bar" -3]]) "li2el3:bari-3eee")))
 
 (ert-deftest bencode--dict ()
   (should (string= (bencode--dict '(("foo" . 5) ("bar" . "baz")))
                    "d3:bar3:baz3:fooi5ee"))
-  (should (string= (bencode--dict '()) "de")))
+  (should (string= (bencode--dict '()) "de"))
+  (should (string= (bencode--dict '(("foo" . [1 2 "bar"])))
+                   "d3:fooli1ei2e3:baree")))

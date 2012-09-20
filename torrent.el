7977dcf54d07f019d815acb790db12325f0a246b
Christopher Wellons <mosquitopsu@gmail.com>
2012-09-20 00:42:58 -0400

    Start creating torrents.

--- torrent.el
+++ torrent.el
@@ -1,15 +1,27 @@
+(require 'cl)
 (require 'bencode)
 
 (defun torrent-info-hash (file)
   "Compute the info_hash for a torrent."
   (sha1 (bencode (cdr (assoc "info" (bdecode-file file))))))
 
+(defun file-size (file)
+  "Return the size of a file."
+  (nth 7 (file-attributes file)))
+
 (defun torrent-compute-pieces (file piece-length)
   "Compute the piece hashes for a file."
-  (let ((length (nth 7 (file-attributes file))))
+  (let ((length (file-size file)))
     (mapconcat 'identity
                (loop for i from 0 below length by piece-length collect
                      (with-temp-buffer
                        (insert-file-contents-literally
                         file nil i (min length (+ i piece-length)))
                        (sha1 (buffer-string) nil nil t))) "")))
+
+(defun* torrent-create-info (file &key (piece-length (ash 1 18)) (name file))
+  "Create an info section for a single-file torrent."
+  `(("length"       . ,(file-size file))
+    ("name"         . ,name)
+    ("piece length" . ,piece-length)
+    ("pieces"       . ,(torrent-compute-pieces file piece-length))))
